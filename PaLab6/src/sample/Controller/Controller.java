4f180415e8fb8e862370ecc73dc6006aedb178ec
teodorzet <75755918+teodorzet@users.noreply.github.com>
2021-03-23 19:16:08 +0200

    Lab6

---
+++ PaLab6/src/sample/Controller/Controller.java
@@ -0,0 +1,133 @@
+package sample.Controller;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import sample.Main;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Controller extends Main {
+
+    @FXML
+    private TextField size;
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private ColorPicker color;
+
+    @FXML
+    private ToggleGroup form;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private RadioButton squareSelect;
+
+    @FXML
+    private RadioButton circleSelect;
+
+    @FXML
+    private RadioButton triangleSelect;
+
+    @FXML
+    private RadioButton lineSelect;
+
+    public void initialize() {
+        GraphicsContext g = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseDragged(e -> {
+            if (lineSelect.isSelected()) {
+                double size = Double.parseDouble(this.size.getText());
+                double x = e.getX() - size / 2;
+                double y = e.getY() - size / 2;
+                g.setFill(color.getValue());
+                g.fillRect(x, y, size, size);
+            }
+        });
+        canvas.setOnMouseClicked(e -> {
+            double size = Double.parseDouble(this.size.getText());
+            double x = e.getX() - size / 2;
+            double y = e.getY() - size / 2;
+            if (circleSelect.isSelected()) {
+                g.setFill(color.getValue());
+                g.fillRoundRect(x, y, size, size, size, size);
+            }
+            if (squareSelect.isSelected()) {
+                g.setFill(color.getValue());
+                g.fillRect(x, y, size, size);
+            }
+        });
+    }
+
+    public void onExit() {
+        Platform.exit();
+    }
+
+    public void saveOn(javafx.event.ActionEvent actionEvent) {
+        try {
+            Image snapshot = canvas.snapshot(null, null);
+
+            ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File("paint.png"));
+        } catch (Exception e) {
+            System.out.println("Failed to save image: " + e);
+        }
+    }
+
+    public void loadOn(ActionEvent actionEvent) {
+        try {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.setInitialDirectory(new File("C:\\Users\\Zetq\\IdeaProjects\\Advanced-Programming\\PaLab6"));
+            File file = fileChooser.showOpenDialog(stage);
+            System.out.println(file.getAbsolutePath());
+
+            BufferedImage tempCard = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(tempCard, null);
+
+            canvas.getGraphicsContext2D().drawImage(image,0,0,image.getWidth(),image.getHeight());
+
+        } catch (Exception e) {
+            System.out.println("Failed to load image: " + e);
+        }
+    }
+
+    public void resetOn(ActionEvent actionEvent) {
+        canvas.getGraphicsContext2D().clearRect(0,0,canvas.getWidth(),canvas.getHeight());
+    }
+
+    public void exitOn(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+}
---
+++ PaLab6/src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Drawing app");
+        primaryStage.setScene(new Scene(root, 662, 465));
+        stage = primaryStage;
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
